4fe528822f785bd3999b0df648acb37f929d7f4e
kkk070 <k7tanaka7@gmail.com>
2021-04-02 12:35:49 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   
   root to: 'tasks#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
   
   resources :tasks
-  
   root to: 'toppages#index'
   
 end
