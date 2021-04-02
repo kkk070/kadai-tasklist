0150002c0ddb5ebb6a6f00de1141c3ea5cd59866
kkk070 <k7tanaka7@gmail.com>
2021-04-02 12:15:19 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
-  root to: 'toppages#index'
   root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
+  
   resources :tasks
+  
+  root to: 'toppages#index'
   
 end
