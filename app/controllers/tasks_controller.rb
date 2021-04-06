f7af7e2ea069591be539561e784fecf3d1000b02
kkk070 <k7tanaka7@gmail.com>
2021-04-06 12:25:22 +0000

    1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
-      @task = current_user.tasks.build  # form_with 用
+      @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
   end
@@ -45,15 +44,12 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'Task を削除しました。'
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   # Strong Parameter
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
+  
+  
   resources :users, only: [:index, :show, :create]
   resources :tasks, only: [:create, :destroy]
   resources :tasks
